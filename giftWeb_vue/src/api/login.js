c9210b9b3e0485429d11763f59672ef04f48a5bc
xierunwei <xierunwei@taqu.cn>
2021-02-25 13:39:42 +0800

    gift

---
+++ giftWeb_vue/src/api/login.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+export function loginByUsername(username, password, code) {
+  const data = {
+    username,
+    password,
+    code
+  }
+  return request({
+    url: '/auth/login',
+    method: 'post',
+    data
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/auth/logout',
+    method: 'post'
+  })
+}
+
+export function getUserInfo(token) {
+  return request({
+    url: '/auth/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+/**
+ * 获取验证码 kaptcha
+ */
+export function getKaptcha() {
+  return request({
+    url: '/auth/kaptcha',
+    method: 'get'
+  })
+}
